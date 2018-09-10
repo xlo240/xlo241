ab65b23e29149ce793d7bb88368e89a8d0152d7e
xlo <xlo@inbox.ru>
2018-09-11 01:06:58 +0300

    first version

---
+++ test.php
@@ -0,0 +1,13 @@
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Слова</title>
+<script src="js/jquery-2.0.3.js"></script>
+</head>
+
+<body bgcolor="#ccc">
+
+<h1>ПРИВЕТ</h1>
+</body>
+</html>
